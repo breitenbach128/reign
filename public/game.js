ebb7eb8961f1e08270862be7b886fa5b7d09412d
Ben Breitenbach <breitenbach128@hotmail.com>
2020-01-19 14:00:19 -0600

    building utility functions for hex grid

--- public/game.js
+++ public/game.js
@@ -14,6 +14,16 @@ window.onload = function() {
      var marker;
      var hexagonGroup;
      var hexTextNames = [];
+     //Resource Enum
+     var resourceTypes = {
+          gold: 0,
+          iron: 1,
+          gems: 2,
+          fur: 3,
+          wood: 4,
+          spices: 5
+     };
+     var resource_img_names = ["icon_gold","icon_iron","icon_gems","icon_fur","icon_wood","icon_spices"];
 
      var config = {
           type: Phaser.AUTO,
@@ -30,12 +40,25 @@ window.onload = function() {
       var game = new Phaser.Game(config);
       
 	function onPreload() {
-		this.load.image("hexagon", "assets/hexagon_tile3.png");
+		this.load.image("background", "assets/background.jpg");
+		this.load.image("hexagon", "assets/hexagon_tile4.png");
           this.load.image("marker", "assets/marker.png");
           this.load.spritesheet("icons", "assets/icons.png",{ frameWidth: 16, frameHeight: 16 });
           this.load.spritesheet("button1", "assets/button1.png",{ frameWidth: 64, frameHeight: 32 });
           this.load.spritesheet("button1_war", "assets/button1_War.png",{ frameWidth: 64, frameHeight: 32 });
           this.load.spritesheet("ui_arrow_war", "assets/ui_arrow_war.png",{ frameWidth: 64, frameHeight: 64 });
+          //Icons
+          
+          this.load.image("icon_gold", "assets/gold-bar.png");
+          this.load.image("icon_iron", "assets/anvil.png");
+          this.load.image("icon_gems", "assets/gems.png");
+          this.load.image("icon_fur", "assets/sheep.png");
+          this.load.image("icon_wood", "assets/wood-beam.png");
+          this.load.image("icon_spices", "assets/powder-bag.png");
+
+          //Terrain
+          this.load.spritesheet("terrain_icons", "assets/terrain_icons.png",{ frameWidth: 32, frameHeight: 32 });
+
 
           console.log("preload completed")
 	}
@@ -47,12 +70,15 @@ window.onload = function() {
           game_width = cv.width;
           game_height = cv.height;
           offsetX = 250;
+
+          //Background
+          this.add.image(game_width/2, game_height/2, 'background').setOrigin(.5).setScale(4);
           // game.scale.scaleMode = Phaser.ScaleManager.SHOW_ALL;
           // game.scale.pageAlignHorizontally = true;
           // game.scale.pageAlignVertically = true;
 
           //DRAW BOARD
-          var board_rect = this.add.rectangle(game_width/2, game_height/2, game_width, game_height, 0x6993b8);
+          var board_rect = this.add.rectangle(game_width/2, game_height/2, game_width, game_height, 0x6993b8).setAlpha(.1);
           board_rect.setInteractive();
 
           hexagonGroup = this.add.group();
@@ -66,7 +92,8 @@ window.onload = function() {
                          let hexagonCoord = {x:Math.floor(j/2),y:(j%2)+(i*2)};
                          hexagon.setup(count,hexagonCoord);
                          hexagonGroup.add(hexagon);
-                         hexTextNames.push(this.add.text(hexagonX+hexagonWidth/2+offsetX, hexagonY+hexagonHeight/2, (j+','+i + ": "+count+" \n"+hexagonCoord.x + ","+hexagonCoord.y), { color: "#000000", fontSize:12, fontFamily: '"xirod"', align:'center' }).setOrigin(.5))
+                         this.add.image(hexagonX+hexagonWidth/2+offsetX,hexagonY+hexagonHeight/2,'terrain_icons',Phaser.Math.Between(0,3));
+                         hexTextNames.push(this.add.text(hexagonX+hexagonWidth/2+offsetX, hexagonY+hexagonHeight/2-32, (j+','+i + ": "+count+" \n"+hexagonCoord.x + ","+hexagonCoord.y), { color: "#000000", fontSize:12, fontFamily: '"xirod"', align:'center' }).setOrigin(.5))
                          console.log(j,i,count,hexagonCoord);
                          count++;
                     }
@@ -85,7 +112,7 @@ window.onload = function() {
           //console.log("Group Sizes:",game_width,game_height,hexagonGroup.x,hexagonGroup.y,hexagonGroup);
 		marker = this.add.sprite(0,0,"marker");
 		marker.setOrigin(0.5);
-		marker.visible=true;//Toggle for testing
+		marker.visible=false;//Toggle for testing
           hexagonGroup.add(marker);
           
           //INPUT TRACKING
@@ -129,6 +156,8 @@ window.onload = function() {
          this.menu_txt_ownership = this.add.text((game_width*(10/16))+10, game_height-112, ' Player1', { stroke:'#000000', strokeThickness:6,fontSize:12, fontFamily: '"xirod"', align:'left' });
 
           //Player Listing
+          this.ui_current_turn = this.add.text(10, 48, ' Turn', { color:'#00FF00', stroke:'#000000', strokeThickness:6,fontSize:32, fontFamily: '"xirod"', align:'left' });
+          this.ui_current_turn = this.add.text(36, 96, ' 1/15', { color:'#00FF00', stroke:'#000000', strokeThickness:6,fontSize:24, fontFamily: '"xirod"', align:'left' });
           this.ui_player_listing_1 = this.add.text(10, 128, ' Player 1', { color:'#00FF00', stroke:'#000000', strokeThickness:6,fontSize:12, fontFamily: '"xirod"', align:'left' });
           this.ui_player_listing_2 = this.add.text(10, 160, ' Player 2', { color:'#FF0000', stroke:'#000000', strokeThickness:6,fontSize:12, fontFamily: '"xirod"', align:'left' });
           this.ui_player_listing_3 = this.add.text(10, 192, ' Player 3', { color:'#0000FF', stroke:'#000000', strokeThickness:6,fontSize:12, fontFamily: '"xirod"', align:'left' });
@@ -158,31 +187,57 @@ window.onload = function() {
           var spriteButton2b = new SpriteButton(this,(game_width*(14/16)),game_height-72,'button1',0,1,2,function(){console.log("Clicked Down")});
           var spriteButton3b = new SpriteButton(this,(game_width*(15/16)),game_height-72,'button1',0,1,2,function(){console.log("Clicked Down")});
 
-          var spriteButton1c = new RectangleButton(this,(game_width*(14/16)),game_height-36,128,32,0x172531,0x172531,0x172531,function(){console.log("Clicked Down")});
+          var spriteButton1c = new RectangleButton(this,(game_width*(14/16)),game_height-36,128,32,0x172531,0x172531,0x172531,function(){console.log("Clicked Down")},"End Turn");
           this.UI_Arrows = [];
 
           //Setup Game
           
+          
+          
           //Give Players Ownership
+          this.playerKingdoms = [];//Selected kingdom quick reference
           let setup_player_list = [1,2,3,4];
           let player_colors = [0x00FF00,0xFF0000,0x0000FF,0xFFFF00];
           kingdomsAvailable = hexagonGroup.getChildren();
           setup_player_list.forEach(function(e){
                let find = true;
                while(find){
-                    let tileSelection = Phaser.Math.Between(0,kingdomsAvailable.length);
-                    if(kingdomsAvailable[tileSelection].owner == -1){
+                    let tileSelection = Phaser.Math.Between(0,kingdomsAvailable.length-1);
+                    //Make sure there is at least one tile gap between kingdoms
+                    let distanceToSelection = 12;
+                    let minDistance = 1;
+                    let distanceCheck = true;
+                    if(this.playerKingdoms.length > 0){
+                         this.playerKingdoms.forEach(function(e){
+                              distanceToSelection = offset_distance(oddr_to_cube(kingdomsAvailable[tileSelection].hex),oddr_to_cube(e.hex));
+                              if(distanceToSelection <= minDistance){
+                                   distanceCheck = false;
+                              }
+                         },this);
+                    }
+                    if(kingdomsAvailable[tileSelection].owner == -1 && distanceCheck){
                          find = false;
                          kingdomsAvailable[tileSelection].owner = e;
                          kingdomsAvailable[tileSelection].setTint(player_colors[e-1]);
-                         console.log("Kingdom Hex to Cube",oddr_to_cube(kingdomsAvailable[tileSelection].hex));
-                         console.log("Distance to 0,0",offset_distance(oddr_to_cube(kingdomsAvailable[tileSelection].hex),oddr_to_cube({x:0,y:0})));
+                         this.playerKingdoms.push(kingdomsAvailable[tileSelection]);
+                         let randResource = Phaser.Math.Between(0,5);
+                         kingdomsAvailable[tileSelection].resource = randResource;
+                         this.add.image(kingdomsAvailable[tileSelection].x,kingdomsAvailable[tileSelection].y+32,resource_img_names[randResource]).setScale(0.05);
                     }
                }
-          });
-          // hexagonGroup.getChildren().forEach(function(e){
+          },this);
 
-          // });
+          //Distribute Random Resources to other kingdoms
+          hexagonGroup.getChildren().forEach(function(e){
+               if(e.owner == -1){
+                    if(Phaser.Math.Between(0,99) < 25){
+                         let randResource = Phaser.Math.Between(0,5);
+                         e.resource = randResource;
+                         this.add.image(e.x,e.y+32,resource_img_names[randResource]).setScale(0.05);
+                         
+                    }
+               }
+          },this);
      }
      function getHexTile(pointer){
           var candidateX = Math.floor((pointer.worldX-hexagonGroup.x)/sectorWidth);
--- public/src/ui.js
+++ public/src/ui.js
@@ -29,32 +29,87 @@ class SpriteButton extends Phaser.GameObjects.Sprite{
 }
 
 class RectangleButton extends Phaser.GameObjects.Rectangle{
-    constructor(scene,x,y,w,h,fillColor,strokeColor,hoverColor,downFunction) {
+    constructor(scene,x,y,w,h,fillColor,strokeColor,hoverColor,downFunction,text) {
 
         super(scene, x, y, w, h, fillColor)
         this.scene = scene;  
         this.scene.add.existing(this);
-        
+        this.fillColor = fillColor;
+        this.strokeColor = strokeColor;
+        this.hoverColor = hoverColor;
         //Setup Interactive 
         this.setInteractive()
-        .on('pointerdown', () => {this.enterButtonDownState();downFunction()})
-        .on('pointerover', () => this.enterButtonHoverState() )
-        .on('pointerout', () => this.enterButtonRestState() )
-        .on('pointerup', () => this.resetButton());
+        .on('pointerdown', () => {this.enterButtonDownState(this.fillColor);downFunction()})
+        .on('pointerover', () => this.enterButtonHoverState(this.hoverColor) )
+        .on('pointerout', () => this.enterButtonRestState(this.fillColor) )
+        .on('pointerup', () => this.resetButton(this.fillColor));
 
+        //Text
+        this.scene.add.text(x, y, text, { color:'#FFFFFF', stroke:'#000000', strokeThickness:6,fontSize:12, fontFamily: '"xirod"', align:'center' }).setOrigin(.5);
         //Reset Timer
 
     }
-    enterButtonDownState(downFunction){        
-        
+    enterButtonDownState(c){        
+        this.setFillStyle(LightenDarkenColorHex(c,-10));
+        console.log("down",c)
     }
-    enterButtonHoverState(){
-        
+    enterButtonHoverState(c){
+        this.setFillStyle(c);
     }
-    enterButtonRestState(){
-        
+    enterButtonRestState(c){
+        this.setFillStyle(c);
+        console.log(c)
     }
-    resetButton(){
-        
+    resetButton(c){
+        this.setFillStyle(c);
+        console.log("up",c)
     }
+}
+
+function LightenDarkenColorString(col,amt) {
+    var usePound = false;
+    if ( col[0] == "#" ) {
+        col = col.slice(1);
+        usePound = true;
+    }
+
+    var num = parseInt(col,16);
+
+    var r = (num >> 16) + amt;
+
+    if ( r > 255 ) r = 255;
+    else if  (r < 0) r = 0;
+
+    var b = ((num >> 8) & 0x00FF) + amt;
+
+    if ( b > 255 ) b = 255;
+    else if  (b < 0) b = 0;
+
+    var g = (num & 0x0000FF) + amt;
+
+    if ( g > 255 ) g = 255;
+    else if  ( g < 0 ) g = 0;
+    var newColor = (usePound?"#":"") + (g | (b << 8) | (r << 16));
+    return newColor.toString(16);
+}
+function LightenDarkenColorHex(col,amt) {
+
+    var num = col;
+
+    var r = (num >> 16) + amt;
+
+    if ( r > 255 ) r = 255;
+    else if  (r < 0) r = 0;
+
+    var b = ((num >> 8) & 0x00FF) + amt;
+
+    if ( b > 255 ) b = 255;
+    else if  (b < 0) b = 0;
+
+    var g = (num & 0x0000FF) + amt;
+
+    if ( g > 255 ) g = 255;
+    else if  ( g < 0 ) g = 0;
+    var newColor = (g | (b << 8) | (r << 16));
+    return newColor;
 }
